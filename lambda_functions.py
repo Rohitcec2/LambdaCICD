0f648a861993b7d18fb30c170adeff7e639870c1
Rohitcec2 <76222356+Rohitcec2@users.noreply.github.com>
2025-08-08 23:20:50 +0530

    Update lambda_functions.py

--- lambda_functions.py
+++ lambda_functions.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Code changes made to CICD for lambda !')
+        'body': json.dumps('Hello from Lambda! Code changes made to CICD for lambda !,I have executed my first codepipeline')
     }
